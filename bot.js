76083b4192a34eba6b8b97da5d0189efb6e7744f
Héctor Fernández Hompanera <Geralt741@gmail.com>
2021-10-22 14:59:45 +0200

    Creation of dice roll bot for discord

---
+++ bot.js
@@ -0,0 +1,57 @@
+const { Client, Intents, MessageEmbed } = require('discord.js');
+const { token } = require('./config.json');
+
+const client = new Client({
+    intents: [
+        Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES,
+        Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+    ],
+});
+
+client.once('ready', () => {
+    console.log('Ready!');
+});
+
+client.on('messageCreate', async message => {
+    if (message.content.startsWith('!')) {
+        const commandRegex = /!(\d+)d(\d+)(\s?(\+|-)\s?(\d+))?/;
+        try {
+            const dice = commandRegex.exec(message.content)[1];
+            const faces = commandRegex.exec(message.content)[2];
+            // Contenido adicional (por ejemplo '+ 30' a la tirada)
+            const operator = commandRegex.exec(message.content)[4];
+            const number = commandRegex.exec(message.content)[5];
+            const extra = operator && number ? eval(operator + number) : null;
+
+            const rollMsg = roll(dice, faces, extra);
+            await message.channel.send({ embeds: [rollEmbed(rollMsg)] });
+        }
+        catch (error) {
+            console.error(error);
+            await message.channel.send({ embeds: [rollEmbed('Formato incorrecto, prueba algo tipo: !1d20 o !3d8 + 3')] });
+        }
+    }
+});
+
+client.login(token);
+
+
+const roll = (dice, faces, extra) => {
+    const rolls = Array.apply(1, Array(parseInt(dice)))
+        .map(() => Math.floor(Math.random() * (faces) + 1));
+
+    const rollSum = rolls.reduce((a, b) => a + b, 0);
+
+    return extra
+        ? parseInt(dice) === 1
+            ? `Tirada: ${rollSum} + (${extra}) = ${rollSum + extra}`
+            : `Tiradas: ${rolls.join(', ')}\nTotal: ${rollSum} + (${extra}) = ${rollSum + extra}`
+        : parseInt(dice) === 1
+            ? `Tirada: ${rollSum}`
+            : `Tiradas: ${rolls.join(', ')}\nTotal: ${rollSum}`;
+};
+
+const rollEmbed = (message) => new MessageEmbed()
+    .setColor('#A01616')
+    .setTitle('Resultado')
+    .setDescription(message);
