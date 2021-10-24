96ddcc8ad2762de632f264a13d5aca06b37a0e3c
Héctor Fernández Hompanera <geralt741@gmail.com>
2021-10-24 19:21:32 +0200

    Style fixes

--- bot.js
+++ bot.js
@@ -83,8 +83,8 @@ const helpEmbed = () => new MessageEmbed
         Permite sumar y restar a la tirada final con los operdores + y -
 
         Ejemplos:
-         Tirar un dado de veinte: !1d20
-         Tirar tres dados de seis: !3d6
-         Tirar dos dados de diez y sumarle cinco al resultado final: !3d6 + 5
+            - Tirar un dado de veinte: !1d20
+            - Tirar tres dados de seis: !3d6
+            - Tirar dos dados de diez y sumarle cinco al resultado final: !3d6 + 5
         \`\`\` 
         `);
