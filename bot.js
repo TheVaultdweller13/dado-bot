21e26244674174cd059aad2708c3f4cbe504d121
Héctor Fernández Hompanera <geralt741@gmail.com>
2021-10-24 19:21:59 +0200

    Minor fix·

--- bot.js
+++ bot.js
@@ -85,6 +85,6 @@ const helpEmbed = () => new MessageEmbed
         Ejemplos:
             - Tirar un dado de veinte: !1d20
             - Tirar tres dados de seis: !3d6
-            - Tirar dos dados de diez y sumarle cinco al resultado final: !3d6 + 5
+            - Tirar dos dados de diez y sumarle cinco al resultado final: !3d10 + 5
         \`\`\` 
         `);
