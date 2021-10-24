caa5e614550b85c0cc5b62f769e10e176d5026dd
Héctor Fernández Hompanera <geralt741@gmail.com>
2021-10-24 19:25:49 +0200

    Minor fixes

--- bot.js
+++ bot.js
@@ -80,11 +80,11 @@ const helpEmbed = () => new MessageEmbed
     .setDescription(`
         \`\`\`
         Para usar el dado virtual escribe el prefijo '!' y a continuación tu tirada de dados. 
-        Permite sumar y restar a la tirada final con los operdores + y -
+        Permite sumar y restar a la tirada final con los operadores + y -
 
         Ejemplos:
             - Tirar un dado de veinte: !1d20
             - Tirar tres dados de seis: !3d6
-            - Tirar dos dados de diez y sumarle cinco al resultado final: !3d10 + 5
+            - Tirar dos dados de diez y sumarle cinco al resultado final: !2d10 + 5
         \`\`\` 
         `);
