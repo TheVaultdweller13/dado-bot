fb903d3e1e3f7e37d08b66f359eed47dd712c2ec
Héctor Fernández Hompanera <geralt741@gmail.com>
2023-06-20 15:09:04 +0200

    chore: Remove mode of config.example.json

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import Bot from './core/bot.js';
 import App from './app/app.js';
 import modes from './app/modes.js';
 
-const bot = new Bot();
-const discord = new App(modes.DISCORD, bot);
+const discord = new App(modes.DISCORD, new Bot());
 
 discord.start();
